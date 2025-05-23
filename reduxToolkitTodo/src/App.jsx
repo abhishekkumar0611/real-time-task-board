ebed850a61c222fe709e68dfdf31bb89a17dbe2b
AbhishekKumar Demo <abhishekkumar1106@gmail.in>
2025-05-23 14:01:33 +0530

    first commit on reduxToolkitTodos app

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>learn about ReduxToolKit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
